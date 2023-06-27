d7995c0ad964fbcdc67d72a2f115ce7b7a43b589
Richard Tia <richie.tia@gmail.com>
2023-06-27 16:59:45 -0700

    feat: add script for converting function yaml to json

---
+++ tools/yaml_to_json.py
@@ -0,0 +1,23 @@
+import json
+from pathlib import Path
+
+import yaml
+
+BASE_DIR = Path(__file__).parent.parent
+JSON_DIR = BASE_DIR / "function_json"
+CASES_DIR = BASE_DIR / "cases"
+FUNCTION_FOLDERS = Path(CASES_DIR).glob("*")
+
+
+for function_folder in FUNCTION_FOLDERS:
+    folder_path = CASES_DIR / function_folder.name
+    json_path = JSON_DIR / function_folder.name
+    Path(json_path).mkdir(parents=True, exist_ok=True)
+    function_yamls = Path(folder_path).rglob("*.yaml")
+    for function_yaml in function_yamls:
+        yaml_file = folder_path / function_yaml.name
+        json_file = json_path / function_yaml.stem
+        with open(yaml_file) as f:
+            dataMap = yaml.safe_load(f)
+            with open(f"{json_file}.json", "w") as outfile:
+                outfile.write(json.dumps(dataMap, indent=4))
