4b31a31399d5af46bbb86be48c8dafb0d2c03ac8
Richard Tia <richie.tia@gmail.com>
2023-06-28 17:59:44 -0400

    feat: add script for converting function yaml to json (#10)
    * feat: add script for converting function yaml to json
    
    * fix: add newline as part of script
    
    * fix: remove json files and try using csafeloader
    
    * fix: mark fp32 as unsupported in postgres addition due to rounding

---
+++ tools/yaml_to_json.py
@@ -0,0 +1,28 @@
+import json
+from pathlib import Path
+
+import yaml
+
+try:
+    from yaml import CSafeLoader as SafeLoader
+except ImportError:
+    from yaml import SafeLoader
+
+BASE_DIR = Path(__file__).parent.parent
+JSON_DIR = BASE_DIR / "function_json"
+CASES_DIR = BASE_DIR / "cases"
+FUNCTION_FOLDERS = Path(CASES_DIR).glob("*")
+
+
+for function_folder in FUNCTION_FOLDERS:
+    folder_path = CASES_DIR / function_folder.name
+    json_path = JSON_DIR / function_folder.name
+    Path(json_path).mkdir(parents=True, exist_ok=True)
+    function_yamls = Path(folder_path).rglob("*.yaml")
+    for function_yaml in function_yamls:
+        yaml_file = folder_path / function_yaml.name
+        json_file = json_path / function_yaml.stem
+        with open(yaml_file) as f:
+            dataMap = yaml.load(f, SafeLoader)
+            with open(f"{json_file}.json", "w") as outfile:
+                outfile.write('{}\n'.format(json.dumps(dataMap, indent=4)))
