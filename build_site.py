28507759a042fff6160d841ed5320f99911e1cbc
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2024-01-03 14:16:28 +0530

    feat: use pathlib instead of os.path

--- build_site.py
+++ build_site.py
@@ -1,23 +1,20 @@
-import os
-import pathlib
 import shutil
+from pathlib import Path
 
 from bft.html.builder import build_site
 
+
 def copy_with_progress(src, dst, copy_function=shutil.copy2):
-    for root, dirs, files in os.walk(src):
-        relative_path = os.path.relpath(root, src)
-        destination_path = os.path.join(dst, relative_path)
+    for source_path in Path(src).rglob('*'):
+        relative_path = source_path.relative_to(src)
+        destination_path = dst / relative_path
 
-        os.makedirs(destination_path, exist_ok=True)
+        if source_path.is_file():
+            destination_path.parent.mkdir(parents=True, exist_ok=True)
+            copy_function(source_path, destination_path)
+            print(f"Copying: {source_path} -> {destination_path}")
 
-        for file in files:
-            source_file = os.path.join(root, file)
-            destination_file = os.path.join(destination_path, file)
-            copy_function(source_file, destination_file)
-            print(f"Copying: {source_file} -> {destination_file}")
-
-root = pathlib.Path(__file__).parent.resolve()
+root = Path(__file__).parent.resolve()
 index = root / "index.yaml"
 dest = root / "dist"
 
@@ -26,9 +23,9 @@ if dest.exists():
     shutil.rmtree(dest)
 
 # Create the destination directory
-os.mkdir(dest)
+dest.mkdir()
 
-build_site(str(index), str(dest))
+build_site(index, dest)
 
 static_content_dir = root / "static_site"
 
