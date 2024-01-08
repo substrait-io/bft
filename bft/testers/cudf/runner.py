ec126e58fd2af3c11db464dbd127bb50f3ad4c46
Richard Tia <richie.tia@gmail.com>
2024-01-08 16:34:12 -0500

    feat: add cudf arithmetic function tests (#44)
    PR adds support for cudf arithmetic functions.
    This includes functions that work on both cudf.Series and cudf.Dataframe objects

--- bft/testers/cudf/runner.py
+++ bft/testers/cudf/runner.py
@@ -1,10 +1,10 @@
-from typing import Dict, NamedTuple
-
 import cudf
+import math
 
 from bft.cases.runner import SqlCaseResult, SqlCaseRunner
 from bft.cases.types import Case, CaseLiteral
 from bft.dialects.types import SqlMapping
+
 
 type_map = {
     "i8": cudf.dtype("int8"),
@@ -22,16 +22,13 @@ def type_to_cudf_dtype(type: str):
     return type_map[type]
 
 
-def literal_to_str(lit: CaseLiteral):
-    return str(lit.value)
-
-
 class CudfRunner(SqlCaseRunner):
     def __init__(self, dialect):
         super().__init__(dialect)
 
     def run_sql_case(self, case: Case, mapping: SqlMapping) -> SqlCaseResult:
         arg_vectors = []
+        arg_values = []
         for arg in case.args:
             dtype = type_to_cudf_dtype(arg.type)
             if dtype is None:
@@ -39,31 +36,48 @@ class CudfRunner(SqlCaseRunner):
                     f"The type {arg.type} is not supported"
                 )
             arg_vectors.append(cudf.Series(arg.value, dtype=dtype))
+            arg_values.append(arg.value)
         if mapping.infix:
             raise Exception("Cudf runner does not understand infix mappings")
 
-        fn = getattr(arg_vectors[0], mapping.local_name)
-
         try:
             if len(arg_vectors) == 1:
-                result = fn()
+                gdf = cudf.DataFrame({"a": arg_values}, dtype=dtype)
+                result = gdf.eval(f"{mapping.local_name}(a)")
             elif len(arg_vectors) == 2:
+                fn = getattr(arg_vectors[0], mapping.local_name)
                 result = fn(arg_vectors[1])
             else:
+                fn = getattr(arg_vectors[0], mapping.local_name)
                 result = fn(arg_vectors[1:])
         except RuntimeError as err:
             return SqlCaseResult.error(str(err))
 
-        if len(result) != 1:
+        if result.empty and case.result.value is None:
+            return SqlCaseResult.success()
+        elif len(result) != 1:
             raise Exception("Scalar function with one row output more than one row")
-        result = result[0]
+        else:
+            result = result[0]
 
         if case.result == "undefined":
             return SqlCaseResult.success()
         elif case.result == "error":
             return SqlCaseResult.unexpected_pass(str(result))
-        else:
-            if result == case.result.value:
+        elif case.result == "nan":
+            if math.isnan(result):
                 return SqlCaseResult.success()
+        else:
+            if case.result.value == result:
+                return SqlCaseResult.success()
+            elif case.result.value == str(result):
+                return SqlCaseResult.success()
+            elif case.result.value is None:
+                if str(result) == "<NA>":
+                    return SqlCaseResult.success()
+                elif result is None:
+                    return SqlCaseResult.success()
+                else:
+                    return SqlCaseResult.mismatch(str(result))
             else:
                 return SqlCaseResult.mismatch(str(result))
