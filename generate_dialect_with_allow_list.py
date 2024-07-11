ad54bad3ff6f4bdcc35716a11181719ebe680f83
Chandra Sanapala <chandra@sundeck.io>
2024-07-11 13:01:38 +0530

    Changes to use uri as the function name

--- generate_dialect_with_allow_list.py
+++ generate_dialect_with_allow_list.py
@@ -16,6 +16,9 @@ def debug(msg):
 
 
 class Substrait:
+    @staticmethod
+    def get_base_uri():
+        return 'https://github.com/substrait-io/substrait/blob/main/extensions/'
 
     @staticmethod
     def get_supported_kernels_from_impls(func):
@@ -25,12 +28,12 @@ class Substrait:
             if 'args' in impl:
                 for arg in impl['args']:
                     if 'value' in arg:
-                        signature += arg['value'] + ","
+                        signature += arg['value'] + "_"
                     else:
                         debug(f"Missing value in arg: {arg['options']} for function: {func['name']}")
                         signature += 'string_'
                 signature = signature[:-1]  # remove the last , from the signature
-            signature += ":" + impl['return']
+            # signature += ":" + impl['return']
             supported_kernels.append(signature)
         return supported_kernels
 
@@ -38,11 +41,13 @@ class Substrait:
     def add_functions_to_map(func_list, function_map, suffix, extension):
         for func in func_list:
             name = func['name']
+            uri = f"{suffix}.{name}"
             if name in function_map:
                 debug(f"Duplicate function name: {name} renaming to {name}_{suffix} extension: {extension}")
                 name = f"{name}_{suffix}"
                 assert name not in function_map
             func['supported_kernels'] = Substrait.get_supported_kernels_from_impls(func)
+            func['uri'] = uri
             func.pop('description', None)
             func.pop('impls', None)
             function_map[name] = func
@@ -53,19 +58,21 @@ class Substrait:
         extensions = []
         for root, dirs, files in os.walk('substrait/extensions'):
             for file in files:
-                if file.endswith('.yaml'):
+                if file.endswith('.yaml') and file.startswith('functions_'):
                     extensions.append(os.path.join(root, file))
 
         extensions.sort()
 
         scalar_functions = {}
         aggregate_functions = {}
+        dependencies = {}
         # convert yaml file to a python dictionary
         for extension in extensions:
             suffix = extension[:-5]  # strip .yaml at the end of the extension
             suffix = suffix[suffix.rfind('/') + 1:]  # strip the path and get the name of the extension
-            suffix = suffix[suffix.rfind('_') + 1:]  # get the suffix after the last _
+            suffix = suffix[suffix.find('_') + 1:]  # get the suffix after the last _
 
+            dependencies[suffix] = Substrait.get_base_uri() + extension
             with open(extension, 'r') as fh:
                 data = yaml.load(fh, Loader=yaml.FullLoader)
                 if 'scalar_functions' in data:
@@ -73,18 +80,15 @@ class Substrait:
                 if 'aggregate_functions' in data:
                     Substrait.add_functions_to_map(data['aggregate_functions'], aggregate_functions, suffix, extension)
 
-        return {'scalar_functions': scalar_functions, 'aggregate_functions': aggregate_functions}
+        return {'dependencies': dependencies, 'scalar_functions': scalar_functions, 'aggregate_functions': aggregate_functions}
 
 
 class Dialect:
     @staticmethod
     def read_dialect(name):
-        dialect = {}
         with open(f'dialects/{name}.yaml', 'r') as fh:
             data = yaml.load(fh, Loader=yaml.FullLoader)
-            dialect['scalar_functions'] = data['scalar_functions']
-            dialect['aggregate_functions'] = data['aggregate_functions']
-        return dialect
+        return data
 
     @staticmethod
     def get_unsupported_kernel_signatures(dialect_func):
@@ -95,9 +99,9 @@ class Dialect:
         for kernel in dialect_func['unsupported_kernels']:
             signature = ""
             for arg_type in kernel['args']:
-                signature += arg_type + ","
-            signature = signature[:-1]  # remove the last , from the signature
-            signature += ":" + kernel['result']
+                signature += arg_type + "_"
+            signature = signature[:-1]  # remove the last _ from the signature
+            # signature += ":" + kernel['result']
             unsupported_kernels.append(signature)
         return unsupported_kernels
 
@@ -119,6 +123,7 @@ class Dialect:
                 continue
             if name in substrait_functions:
                 func['supported_kernels'] = Dialect.get_supported_kernels_in_func(func, substrait_functions[name])
+                func['name'] = substrait_functions[name]['uri']
             else:
                 error(f"Function {name} not found in substrait functions")
             func.pop('unsupported_kernels', None)
@@ -126,33 +131,46 @@ class Dialect:
         return functions
 
 
-def convert_dialect_to_allow_list(dialect_funcs, substrait_functions):
-    if 'scalar_functions' in dialect_funcs:
-        dialect_funcs['scalar_functions'] = Dialect.get_supported_functions_with_supported_kernels(
-            dialect_funcs['scalar_functions'], substrait_functions['scalar_functions'])
-    if 'aggregate_functions' in dialect_funcs:
-        dialect_funcs['aggregate_functions'] = Dialect.get_supported_functions_with_supported_kernels(
-            dialect_funcs['aggregate_functions'], substrait_functions['aggregate_functions'])
-    return dialect_funcs
+def convert_dialect_to_allow_list(dialect, substrait_map):
+    scalar_functions = []
+    aggregate_functions = []
+    if 'scalar_functions' in dialect:
+        scalar_functions = Dialect.get_supported_functions_with_supported_kernels(
+            dialect['scalar_functions'], substrait_map['scalar_functions'])
+    if 'aggregate_functions' in dialect:
+        aggregate_functions = Dialect.get_supported_functions_with_supported_kernels(
+            dialect['aggregate_functions'], substrait_map['aggregate_functions'])
+    dialect.pop('scalar_functions', None)
+    dialect.pop('aggregate_functions', None)
+    dialect['dependencies'] = substrait_map['dependencies']
+    dialect['scalar_functions'] = scalar_functions
+    dialect['aggregate_functions'] = aggregate_functions
+    return dialect
 
 
-def process_one_dialect(substrait_functions, dialect_name):
+def process_one_dialect(substrait_map, dialect_name):
     dialect_funcs = Dialect.read_dialect(dialect_name)
-    dialect_funcs = convert_dialect_to_allow_list(dialect_funcs, substrait_functions)
+    dialect_funcs = convert_dialect_to_allow_list(dialect_funcs, substrait_map)
     print(
         f"{dialect_name} total scalar functions: {len(dialect_funcs['scalar_functions'])} "
         f"total aggregate functions: {len(dialect_funcs['aggregate_functions'])}")
 
     # dump the dialect functions with supported kernels to a file in the same order as in the dictionary
-    with open(f'out_{dialect_name}_funcs.yaml', 'w') as fh:
+    with open(f'dialects_new/{dialect_name}.yaml', 'w') as fh:
         yaml_obj.dump(dialect_funcs, fh)
 
 
-substrait_funcs = Substrait.read_substrait_extensions()
+substrait_spec = Substrait.read_substrait_extensions()
 print(
-    f"Substrait total scalar functions: {len(substrait_funcs['scalar_functions'])} "
-    f"total aggregate functions: {len(substrait_funcs['aggregate_functions'])}")
+    f"Substrait total scalar functions: {len(substrait_spec['scalar_functions'])} "
+    f"total aggregate functions: {len(substrait_spec['aggregate_functions'])}")
 with open('out_substrait_funcs.yaml', 'w') as f:
-    yaml_obj.dump(substrait_funcs, f)
+    yaml_obj.dump(substrait_spec, f)
 
-process_one_dialect(substrait_funcs, 'duckdb')
+process_one_dialect(substrait_spec, 'cudf')
+process_one_dialect(substrait_spec, 'datafusion')
+process_one_dialect(substrait_spec, 'duckdb')
+process_one_dialect(substrait_spec, 'postgres')
+process_one_dialect(substrait_spec, 'snowflake')
+process_one_dialect(substrait_spec, 'sqlite')
+process_one_dialect(substrait_spec, 'velox_presto')
