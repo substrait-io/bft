e457598438394b7c9b148c9750eabd3b88033411
Chandra Sanapala <chandra@sundeck.io>
2024-07-15 04:18:07 +0530

    Modify dialect loading code to handle allow list of signatures and short types

--- bft/dialects/types.py
+++ bft/dialects/types.py
@@ -7,6 +7,57 @@ from bft.cases.types import Case, CaseLiteral, Literal, case_to_kernel_str
 from bft.core.function import Kernel
 
 
+type_to_short_type = {
+    'required enumeration': 'req',
+    'i8': 'i8',
+    'i16': 'i16',
+    'i32': 'i32',
+    'i64': 'i64',
+    'fp32': 'fp32',
+    'fp64': 'fp64',
+    'string': 'str',
+    'binary': 'vbin',
+    'boolean': 'bool',
+    'timestamp': 'ts',
+    'timestamp_tz': 'tstz',
+    'date': 'date',
+    'time': 'time',
+    'interval_year': 'iyear',
+    'interval_day': 'iday',
+    'uuid': 'uuid',
+    'fixedchar<N>': 'fchar',
+    'varchar<N>': 'vchar',
+    'fixedbinary<N>': 'fbin',
+    'decimal<P,S>': 'dec',
+    'precision_timestamp<P>': 'pts',
+    'precision_timestamp_tz<P>': 'ptstz',
+    'struct<T1,T2,...,TN>': 'struct',
+    'list<T>': 'list',
+    'map<K,V>': 'map',
+    'map': 'map',
+    'any': 'any',
+    'any1': 'any1',
+    'any2': 'any2',
+    'any3': 'any3',
+    'user defined type': 'u!name',
+
+    # added to handle parametrized types
+    'fixedchar': 'fchar',
+    'varchar': 'vchar',
+    'fixedbinary': 'fbin',
+    'decimal': 'dec',
+    'precision_timestamp': 'pts',
+    'precision_timestamp_tz': 'ptstz',
+    'struct': 'struct',
+    'list': 'list',
+
+    # added to handle geometry type
+    'geometry': 'geometry',
+}
+
+short_type_to_type = {st: lt for lt, st in type_to_short_type.items()}
+
+
 class DialectKernel(NamedTuple):
     arg_types: List[str]
     result_type: str
@@ -22,7 +73,8 @@ class DialectFunction(NamedTuple):
     unsupported: bool
     extract: bool
     required_options: Dict[str, str]
-    unsupported_kernels: List[DialectKernel]
+    variadic_min: int
+    supported_kernels: List[DialectKernel]
 
 
 class DialectFile(NamedTuple):
@@ -30,6 +82,7 @@ class DialectFile(NamedTuple):
     type: str
     scalar_functions: List[DialectFunction]
     aggregate_functions: List[DialectFunction]
+    uri_to_func_prefix: Dict[str, str]
 
 
 class SqlMapping(NamedTuple):
@@ -53,37 +106,47 @@ class Dialect(object):
         self.__aggregate_functions_by_name: Dict[str, DialectFunction] = {
             f.name: f for f in dialect_file.aggregate_functions
         }
+        self.__func_prefixes: Dict[str, str] = {
+            uri: prefix for uri, prefix in dialect_file.uri_to_func_prefix.items()
+        }
 
     def __supports_case_kernel(
-        self,
-        dfunc: DialectFunction,
-        args: List[CaseLiteral],
-        result: CaseLiteral | Literal["error", "undefined"],
+            self,
+            dfunc: DialectFunction,
+            args: List[CaseLiteral],
+            result: CaseLiteral | Literal["error", "undefined"],
     ):
-        for unsupported_kernel in dfunc.unsupported_kernels:
+        arg_len_matched = False
+        for supported_kernel in dfunc.supported_kernels:
             if dfunc.aggregate:
                 arg_len = 1
             else:
                 arg_len = len(args)
-            if len(unsupported_kernel.arg_types) != arg_len:
-                raise Exception(
-                    "Unreachable path.  Unsupported kernel with different # of types than case"
-                )
+            if len(supported_kernel.arg_types) != arg_len and dfunc.variadic_min == -1:
+                continue
+            arg_len_matched = True
             matched = True
-            for ktype, arg in zip(unsupported_kernel.arg_types, args):
-                if arg.type != ktype:
+            kernel_arg_types = supported_kernel.arg_types
+            if dfunc.variadic_min != -1 and len(supported_kernel.arg_types) == 1:
+                kernel_arg_types = supported_kernel.arg_types * len(args)
+
+            any_map = {}
+            for ktype, arg in zip(kernel_arg_types, args):
+                if arg.type != ktype and not ktype.startswith("any"):
                     matched = False
                     break
+                if ktype.startswith("any"):
+                    if ktype not in any_map:
+                        any_map[ktype] = arg.type
+                    elif any_map[ktype] != arg.type:
+                        matched = False
+                        break
             if matched:
-                if (
-                    hasattr(result, "type")
-                    and unsupported_kernel.result_type != result.type
-                ):
-                    raise Exception(
-                        "Unreachable path.  Unsupported kernel matches arg types but not result type"
-                    )
-                return f"The dialect {self.name} does not support the kernel {case_to_kernel_str(dfunc.name, args, result)}"
-        return None
+                return None
+
+        if not arg_len_matched:
+            raise Exception("Unreachable path.  Supported kernel with different # of types than case")
+        return f"The dialect {self.name} does not support the kernel {case_to_kernel_str(dfunc.name, args, result)}"
 
     def __supports_options(self, dfunc: DialectFunction, case: Case):
         for case_opt, case_val in case.options:
@@ -105,31 +168,34 @@ class Dialect(object):
         dfunc = self.__scalar_functions_by_name.get(function_name, None)
         if dfunc is None:
             return False
-        for unsupported_kernel in dfunc.unsupported_kernels:
-            if len(unsupported_kernel.arg_types) != len(kernel.arg_types) and len(
-                unsupported_kernel.arg_types
-            ) < int(kernel.variadic):
-                raise Exception(
-                    "Unreachable path.  Unsupported kernel with different # of types than official kernel"
-                )
+        for supported_kernel in dfunc.supported_kernels:
+            if len(supported_kernel.arg_types) != len(kernel.arg_types):
+                continue
             matched = True
-            for ktype, arg_type in zip(unsupported_kernel.arg_types, kernel.arg_types):
+            for ktype, arg_type in zip(supported_kernel.arg_types, kernel.arg_types):
                 if arg_type != ktype:
                     matched = False
                     break
             if matched:
-                return False
-        return True
+                return True
+        return False
+
+    def _get_function_name(self, case: Case) -> str:
+        prefix = self.__func_prefixes.get(case.base_uri, "")
+        if len(prefix) > 0:
+            return prefix + "." + case.function
+        return case.function
 
     def mapping_for_case(self, case: Case) -> SqlMapping:
-        dfunc_scalar = self.__scalar_functions_by_name.get(case.function, None)
-        dfunc_aggregate = self.__aggregate_functions_by_name.get(case.function, None)
+        func_name = self._get_function_name(case)
+        dfunc_scalar = self.__scalar_functions_by_name.get(func_name, None)
+        dfunc_aggregate = self.__aggregate_functions_by_name.get(func_name, None)
         dfunc = dfunc_scalar or dfunc_aggregate
         if "PYTEST_CURRENT_TEST" not in os.environ:
             if dfunc is None:
                 return None
-        elif dfunc.unsupported:
-            pytest.skip("Skipping unsupported function.")
+        elif dfunc is None:
+            pytest.skip(f"Skipping unsupported function. {case.base_uri}/{case.function}")
 
         kernel_failure = self.__supports_case_kernel(dfunc, case.args, case.result)
         if kernel_failure is not None:
